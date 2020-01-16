3ae072a657f7efecf29cebd09ec03cb4ab5316a2
Bill Kirtley <bill.kirtley@gmail.com>
2020-01-16 14:13:47 -0500

    Use mailgun to deliver email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,18 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+
+  # Do care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_host: '127.0.0.1:8025',
+    api_key: 'fake-api-key',
+    domain: 'example.com',
+    api_ssl: false
+  }
 
   config.action_mailer.perform_caching = false
 
